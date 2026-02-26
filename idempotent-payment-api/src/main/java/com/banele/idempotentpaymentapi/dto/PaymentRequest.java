8f9dd224badfc475f75b5d4822f0a428a5d75157
Banele Mdhluli <139925146+banelemdhluli@users.noreply.github.com>
2026-02-26 08:37:07 +0200

    Updated PaymentRequest.java

--- idempotent-payment-api/src/main/java/com/banele/idempotentpaymentapi/dto/PaymentRequest.java
+++ idempotent-payment-api/src/main/java/com/banele/idempotentpaymentapi/dto/PaymentRequest.java
@@ -1,26 +1,24 @@
 package com.banele.idempotentpaymentapi.dto;
 
 import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
+import jakarta.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
 public class PaymentRequest {
 
-    // Instance variables and their annotations
-
-    @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[a-zA-Z])[a-zA-Z0-9]+$",
+            message = "Request ID must contain letters and numbers combined (no symbols,no numbers only), and cannot be empty"
+    )
     private String requestId;
 
-    @NotNull
-    @DecimalMin("0.01")
+    @DecimalMin(
+            value = "0.01",
+            message = "Amount must be greater than 0.01"
+    )
     private BigDecimal amount;
 
-    public PaymentRequest() {
-    }
-
-    //  Getters & Setters
+    public PaymentRequest() {}
 
     public String getRequestId() {
         return requestId;
